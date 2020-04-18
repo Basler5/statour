52b28f86725908da5345250ba53807aa8097a9d2
Basler <basler5@yandex.ru>
2020-04-18 12:32:09 +0300

    one

--- analizatortour/analizatortour/settings.py
+++ analizatortour/analizatortour/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0e!fcj*f_mq!_2^yc=@8vu0q&m=tl7ci41js0inoz^-q9!e4md'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
